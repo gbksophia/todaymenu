521ba9c50a63f41d77c52b18b7743713eb002c8a
mj <sidewinder87@gmail.com>
2019-08-21 14:24:29 +0900

    api key, 카카오지도 테스트, 초기세팅 일부 커밋

---
+++ eatoday/src/eatoday/bean/APITest.java
@@ -0,0 +1,19 @@
+package eatoday.bean;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/apitest/")
+public class APITest {
+		
+	@RequestMapping("mapapi2.do")
+	public String mapapi2(HttpServletRequest request) {
+		String kwd = request.getParameter("kwd");
+		
+		request.setAttribute("kwd", kwd);
+		return "/apitest/mapapi2";
+	}
+}
