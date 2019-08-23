8e1d6a7c1ac9c5dc906ba16b109588bfcc51b4ba
mj <sidewinder87@gmail.com>
2019-08-23 11:47:10 +0900

    csv -> sql

--- eatoday/src/eatoday/bean/csvTest.java
+++ eatoday/src/eatoday/bean/csvTest.java
@@ -86,45 +86,28 @@ public class csvTest {
 	
 	//http://localhost:8080/eatoday/csvtest/csvdb.do
 	@RequestMapping("csvdb.do")
-	public String csvdb() throws Exception {
-		TestVO tbcvo = new TestVO();
-		RConnection conn = new RConnection();
+	public String csvdb(HttpServletRequest request) throws Exception {
 		try {
+			request.setCharacterEncoding("UTF-8");
+			TestVO tbcvo = new TestVO();
+			RConnection conn = new RConnection();
 			REXP tbc = conn.eval("tbc <- read.csv('D:/R/2bc.csv')");
 			RList list = tbc.asList();
-					
-			for (int i=0; i<list.at(0).length()+1; i++) {
-				REXP te = conn.eval("tbc$text["+i+"]");
-				String [] tex= te.asStrings();
-				List<String> text= new ArrayList<String>();
-				for (String tt : tex) {
-					System.out.println(tt);
-					text.add(tt);
-					tbcvo.setText(tt);
-				}
-				
-				REXP ma = conn.eval("tbc$mate["+i+"]");
-				String [] mat= ma.asStrings();
-				List<String> mate= new ArrayList<String>();
-				for (String mm : mat) {
-					System.out.println(mm);
-					mate.add(mm);
-					tbcvo.setMate(mm);
-				}
-				
-				REXP p = conn.eval("tbc$pro["+i+"]");
-				String [] pr= p.asStrings();
-				List<String> pro= new ArrayList<String>();
-				for (String pp : pr) {
-					System.out.println(pp);
-					pro.add(pp);
-					tbcvo.setPro(pp);
-				}
-				System.out.println();
-				sql.insert("eatoday.insert", tbcvo);
+			
+			String [][] s = new String[list.size()][];
+			for (int i=0; i<list.size(); i++) {
+				s[i] = list.at(i).asStrings();
 			}
 			
-		conn.close();
+			for(int j=0; j<list.at(0).length(); j++) {
+				tbcvo.setText(s[1][j]);
+				tbcvo.setMate(s[2][j]);
+				tbcvo.setPro(s[3][j]);
+				sql.insert("eatoday.insert", tbcvo);
+			}
+			System.out.println("CSV -> SQL");
+
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- eatoday/src/eatoday/vo/TestVO.java
+++ eatoday/src/eatoday/vo/TestVO.java
@@ -6,34 +6,29 @@ public class TestVO {
 	private String text;
 	private String mate;
 	private String pro;
-	
 	public int getNum() {
 		return num;
 	}
 	public void setNum(int num) {
 		this.num = num;
 	}
-	
 	public String getText() {
 		return text;
 	}
 	public void setText(String text) {
 		this.text = text;
 	}
-	
 	public String getMate() {
 		return mate;
 	}
 	public void setMate(String mate) {
 		this.mate = mate;
 	}
-	
 	public String getPro() {
 		return pro;
 	}
 	public void setPro(String pro) {
 		this.pro = pro;
 	}
-	
 
 }
