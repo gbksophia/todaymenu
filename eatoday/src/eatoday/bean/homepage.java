3be60795fd3021803f4c2e28735a3765a9d467d3
mj <sidewinder87@gmail.com>
2019-08-26 15:43:35 +0900

    링크수정

--- eatoday/src/eatoday/bean/homepage.java
+++ eatoday/src/eatoday/bean/homepage.java
@@ -10,11 +10,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class homepage {
 	
 	@RequestMapping("index.do")
-	public String kakaologin(HttpServletRequest request) {
-		String kwd = request.getParameter("kwd");
-		
-		request.setAttribute("kwd", kwd);
+	public String index() {
 		return "/homepage/index";
+	}
+	
+	@RequestMapping("about.do")
+	public String about() {
+		return "/homepage/about";
+	}
+	
+	@RequestMapping("blog-single.do")
+	public String blogSingle() {
+		return "/homepage/blog-single";
+	}
+	
+	@RequestMapping("blog.do")
+	public String blog() {
+		return "/homepage/blog";
+	}
+	
+	@RequestMapping("cart.do")
+	public String cart() {
+		return "/homepage/cart";
+	}
+	
+	@RequestMapping("checkout.do")
+	public String checkout() {
+		return "/homepage/checkout";
+	}
+	
+	@RequestMapping("contact.do")
+	public String contact() {
+		return "/homepage/contact";
+	}
+	
+	@RequestMapping("menu.do")
+	public String menu() {
+		return "/homepage/menu";
+	}
+	
+	@RequestMapping("product-single.do")
+	public String productSingle() {
+		return "/homepage/product-single";
+	}
+	
+	@RequestMapping("services.do")
+	public String services() {
+		return "/homepage/services";
+	}
+	
+	@RequestMapping("shop.do")
+	public String shop() {
+		return "/homepage/shop";
 	}
 	
 }
